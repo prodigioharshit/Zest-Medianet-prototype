63fd9c9aa07cd53c1082dbef53430dca251e1b7f
prodigioharshit <47027841+prodigioharshit@users.noreply.github.com>
2025-12-26 23:14:11 +0530

    restored to previous version

--- index.tsx
+++ index.tsx
@@ -1,2 +1,16 @@
-// This project has been refactored into a single-file plain JavaScript application.
-// Please refer to index.html for the source code.
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+
+const rootElement = document.getElementById('root');
+if (!rootElement) {
+  throw new Error("Could not find root element to mount to");
+}
+
+const root = ReactDOM.createRoot(rootElement);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
